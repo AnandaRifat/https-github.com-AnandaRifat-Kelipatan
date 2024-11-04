e716ca6668041715ad8320f3550d8503ef90b101
AnandaRifat <03rifat05@gmail.com>
2024-11-04 11:02:51 +0700

    Add files via upload

---
+++ KELIPATAN_18_23176004_RIFAT.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg23176004_anandarifat_si3;
+
+/**
+ * NAMA: ANANDA RIFAT AULIA
+ * NIM: 23176004
+ * PRODI: SISTEM INFORMASI
+ * SEMESTER: 3
+ * DESKRIPSI PROGRAM: PROGRAM INI BERISI TENTANG MENCETAK BILANGAN KELIPATAN 3,5
+ * @author ananda rifat aulia
+ */
+public class KELIPATAN_18_23176004_RIFAT {
+    public static void main(String[] args) {
+        double Bilangan = 3.5;
+        // Mencetak kelipatan 3.5
+        while (Bilangan <= 35.0) {
+            System.out.println("x: " + Bilangan);
+            Bilangan += 3.5; // Tambah 3.5 pada setiap iterasi
+        }
+        System.out.println("Halo Aku Ananda Rifat Aulia");
+    }
+}
+
